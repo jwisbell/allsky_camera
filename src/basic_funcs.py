4ca62eb49d17c0b9c2f509735b93e7d4db939287
MasReed <bluekitdrummer@gmail.com>
2018-04-09 15:24:05 -0500

    alt az projection basics

--- src/basic_funcs.py
+++ src/basic_funcs.py
@@ -1,9 +1,8 @@
-
-
 import time
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import matplotlib.lines as lines
 import matplotlib.cm as cm
 import astropy.io.fits as pyfits
 from astropy import units as u
@@ -14,10 +13,11 @@ from astropy.visualization import astropy_mpl_style
 #plt.style.use(astropy_mpl_style)
 
 filepath = 'C://Users/Mason/Desktop/AstroLAB/allsky/'
-filename1 = 'tesCurrentImage.fit'
-#filename1 = 'testimg.fit'
+#filename1 = 'tesCurrentImage.fit'
+filename1 = 'testimg.fit'
 
 VAO = EarthLocation(lat=41.6611*u.deg, lon=-91.5302*u.deg, height=225*u.m)
+xyimgcenter = (320, 240)
 
 def rawFits(filename):
     f = pyfits.open(filename)
@@ -27,37 +27,52 @@ def rawFits(filename):
     return h, d
 
 def getTime(header):
+    """
     if time.localtime().tm_isdst > 0:
         utcoffset = 5*u.hour
     else:
         utcoffset = 6*u.hour
-    t = Time(np.array(header['DATE-OBS']), format='fits', scale='utc') - utcoffset
+    """
+    t = Time(np.array(header['DATE-OBS']), format='fits', scale='utc')# - utcoffset
     return t
 
 def altradius(coords):
-    deltax = abs(300 - coords[0])
-    deltay = abs(245 - coords[1])
+    deltax = abs(xyimgcenter[0] - coords[0])
+    deltay = abs(xyimgcenter[1] - coords[1])
     radius = np.sqrt(deltax**2. + deltay**2.)
     return radius
 
 
+def getangle(line2D):
+    line2Dxy = line2D.get_xydata()
+    angle = np.degrees(np.arctan2((line2Dxy[1][1] - line2Dxy[0][1]), float(line2Dxy[1][0] - line2Dxy[0][0])))
+    return angle
+
+
+
 #####################################################################################
 h1 = rawFits(filepath+filename1)[0]
-d1 = rawFits(filepath+filename1)[1]
-time = getTime(h1)
-print(time)
+d1 = np.rot90((rawFits(filepath+filename1)[1]))
+t1 = getTime(h1)
+print(t1)
 
 
-starxy = [ ['polaris', (459, 246)],
-           ['sirius', (102, 270)],
-           ['pollux', (253, 219)],
-           ['castor', (265, 228)],
-           ['betelgeuse', (189, 313)]
+starxy = [ ['rigel', (518, 337)],
+           ['sirius', (465, 421)],
+           ['procyon', (450, 283)],
+           ['betelgeuse', (423, 374)],
+           ['polaris', (167, 251)],
+           ['capella', (284, 349)],
+           ['castor', (359, 280)],
+           ['pollux', (373, 273)],
+           ['aldebaran', (360, 426)],
+           ['alnilam', (447, 397)]
            ]
 
+d1 = np.flipud(d1)
 
-projection = [ ['horizon', (300, 246), 290],
-               ['zenith', (300, 246), 2]
+projection = [ ['horizon', xyimgcenter, 296],
+               ['zenith', xyimgcenter, 2]
                ]
 
 
@@ -70,40 +85,73 @@ ax.cla()
 altitudes = []
 pixelalts = []
 
+azimuths = []
+pixelazs = []
+
+test = []
+
 for star in starxy:
-    #print(SkyCoord.from_name(star[0]))
-    #print(SkyCoord.from_name(star[0]).transform_to(AltAz(obstime=time, location=VAO)))
-    star.append('{0.alt:.3}'.format(SkyCoord.from_name(star[0]).transform_to(AltAz(obstime=time, location=VAO))))
-    star.append('{0.az:.3}'.format(SkyCoord.from_name(star[0]).transform_to(AltAz(obstime=time, location=VAO))))
+    star.append('{0.alt:.3}'.format(SkyCoord.from_name(star[0]).transform_to(AltAz(obstime=t1, location=VAO))))
+    star.append('{0.az:.3}'.format(SkyCoord.from_name(star[0]).transform_to(AltAz(obstime=t1, location=VAO))))
     projection.append([ star[0]+'Alt', star[1], altradius(star[1])])
     ax.add_artist(patches.Circle(star[1], 2, color='b', fill=False))
+    meridian = lines.Line2D([xyimgcenter[0], star[1][0]],
+                             [xyimgcenter[1], star[1][1]],
+                             lw=1, linestyle='--', color='m', axes=ax)
+    test.append(meridian)
+    ax.add_line(meridian)
     altitudes.append(star[2])
+    azimuths.append(star[3])
 
 
 for marker in projection:
-    if marker[1][0] < 300:
-        pixelalts.append(300 - marker[1][0])
-    else:
-        pixelalts.append(marker[2])
-    ax.add_artist(patches.Circle((300, 246), marker[2], color='m', linestyle='--', fill=False, alpha=.25))
+    pixelalts.append(marker[2])
+    ax.add_artist(patches.Circle(xyimgcenter, marker[2], color='r', linestyle='--', fill=False, alpha=.25))
 
 
-print(projection)
-altitudes2 = [0.00, 90.0]
+
+#print(projection)
+altitudes2 = [19.65, 90.0]
+azimuths2 = []
 
 for string in altitudes:
     string=string[:-4]
     altitudes2.append(float(string))
 
-print(starxy)
-print(altitudes2)
-print(pixelalts)
+for string in azimuths:
+    string=string[:-4]
+    azimuths2.append(float(string))
+
+#print(starxy)
+print()
+print('alt (deg): ', altitudes2)
+print('alt (px): ', pixelalts)
+print()
+print('az (deg): ', azimuths2)
+
+
+
+for i in test:
+    #print(getangle(i))
+    pixelazs.append(getangle(i))
+
+print('pixaz (deg): ', pixelazs)
+
+
 
 plt.imshow(d1, cmap = colmap, origin='lower')
 #plt.show()
 
 plt.figure(2)
+plt.title('Altitude as a function of pixel distance')
 plt.xlabel('pixels from zenith')
-plt.ylabel('degrees')
+plt.ylabel('altitude (deg')
 plt.scatter(pixelalts, altitudes2)
+
+plt.figure(3)
+plt.title("Azimuth as a function of angle")
+plt.xlabel('angle from zenith || x-axis (deg)')
+plt.ylabel('azimuth (deg)')
+plt.scatter(pixelazs, azimuths2)
+
 plt.show()
