73ca55d42565b4c1dd10e159bc17c42e611384f9
Jacob Isbell <jacob.isbell@uiowa.edu>
2018-04-24 18:49:37 -0500

    Fixed lst conversion

--- src/conversions.py
+++ src/conversions.py
@@ -38,41 +38,47 @@ def utc_to_lst(time,hours=False):
 	#time is in format given by FITS header
 	#need the julian date
 	t = Time(time,format='isot',scale='utc')
+	#print t.sidereal_time('apparent', longitude=-1*lon).deg
 	jd = t.jd
-	#using navy conversion accurate to 0.1 seconds
-	d = jd - 2451545.0
-	GMST = 18.697374558 + 24.06570982441908*d
-	gmst = GMST
-	#convert to 24hr clock
-	while gmst > 360:
-		gmst -= 360
 
+	ut = (jd - int(jd) - .5) * 24
+	#using navy conversion accurate to 0.1 seconds
+	T=(jd-2451545.0)/36525.0
+	T0 = 6.697374558+(2400.051336*T)+(0.000025862*T**2)+(ut*1.0027379093)
+	gmst = T0
+
+	while gmst > 24:
+		gmst -= 24
+	lst = gmst + (-1*lon / 15)
 	if hours:
 		#convert degrees to hours
 		gmst = gmst / 15.
 		local_sidereal_time = gmst - lon/15.
 		return local_sidereal_time
-	return gmst - lon
+	return lst/24.*360#, t.sidereal_time('apparent', longitude=-1*lon).deg#gmst - lon
 
 
 def radec_to_altaz(ra,dec,time,debug=False):
 	if debug:
-		print ra, dec,time
+		print ra, dec, time
 	#NOTE -- numpy trig is all in radians
-	RA = np.radians(ra)
-	DEC = np.radians(dec)
+	RA = np.deg2rad(ra)#np.radians(ra)
+	DEC = np.deg2rad(dec)#np.radians(dec)
 	#hour angle
-	H = utc_to_lst(time) - ra #time must be LST
+	H = time - ra#utc_to_lst(time) - ra#time must be LST 
 
 	#now get altitude
-	alt = np.arcsin( np.sin(DEC)*np.sin(LAT) + np.cos(DEC)*np.cos(LAT)*np.cos(np.radians(H)) )
+	alt = np.arcsin( np.sin(DEC)*np.sin(LAT) + np.cos(DEC)*np.cos(LAT)*np.cos(np.deg2rad(H)) )
 
 	#azumith
-	az = np.arccos(  (np.sin(DEC) - np.sin(LAT)*np.sin(alt))/(np.cos(LAT) *np.cos(alt) ) )
+	cos_az = (np.sin(DEC) - np.sin(LAT)*np.sin(alt)) / (np.cos(LAT)*np.cos(alt))
+	az = np.arccos( cos_az )#*np.sign(cos_az)
+	if ra < time:
+		az = 2*np.pi - az
 	if debug:
-		print alt, az
+		print np.rad2deg(alt), np.rad2deg(az)
 
-	return np.degrees(alt), np.degrees(az)
+	return np.rad2deg(alt), np.rad2deg(az)
 
 
 def altaz_to_radec(alt,az,time):
@@ -85,7 +91,7 @@ def altaz_to_radec(alt,az,time):
 	#next the hour angle
 	H = np.arcsin( -1 * np.sin(AZ)*np.cos(ALT) / np.cos(DEC)  )
 
-	RA = utc_to_lst(time) - H
+	RA = time-H#utc_to_lst(time) - H
 
 	return np.degrees(RA), np.degrees(DEC)
 
@@ -101,13 +107,12 @@ def altaz_to_xy(alt, az, imgcenter=(320., 240.), pxpdeg=-3.3, b=0., az_rot = 8.)
 def centroid(image, x,y,s, verbose=False):
 	#calculate the centroid of the peak within a radius
 	x = int(x); y = int(y); s = int(s)
-	print x, y, s
 	region = image[x-s/2:x+s/2, y-s/2:y+s/2]
 	I = []
 	J = []
 	for i in range(s):
 		ival = np.sum(region[i,:])
-		jval = np.sum(region[:,i])
+		jval = np.sum(region[:,i-1])
 		I.append(ival)
 		J.append(jval)
 	mean_i = np.sum(I) / s
@@ -132,25 +137,24 @@ def centroid(image, x,y,s, verbose=False):
 
 
 
-def altaz_grid(im, spacing=10, verbose=False):
+def altaz_grid(im, spacing=15, verbose=False,alph=0.4,north=8):
 	#spacing in degrees
-	north = 8
-	alts = np.linspace(0,100,spacing)
+	color = 'red'
+	alts = np.arange(0,85+spacing,spacing)
 	azs = np.zeros(alts.shape)#np.linspace(0+north,360+north,spacing)
 
 	#need a circle at each alt, and radial lines at each az
 	x,y = altaz_to_xy(alts, azs)
-	print x, y
-
+	
 	circs = []
 	for radius in np.sqrt( np.power(x-320,2.) + np.power(y-240,2.)):
-		c = Circle( (480/2,640/2),radius=radius,fc='none',ec='gray',alpha=0.2)
+		c = Circle( (480/2,640/2),radius=radius,fc='none',ec=color,alpha=alph)
 		circs.append(c)
 
 	#make the lines
 	length=np.max( np.sqrt( np.power(x-320,2.) + np.power(y-240,2.)) )
 	lines = []
-	azs = np.arange(0+north,360+north,15)
+	azs = np.arange(0+north,360+north,30)
 	for az in azs:
 		lines.append(  ([480/2, 480/2+length*np.cos(np.radians(az))], [640/2,640/2+length*np.sin(np.radians(az))]) )
 
@@ -162,7 +166,7 @@ def altaz_grid(im, spacing=10, verbose=False):
 			ax.add_patch(c)
 
 		for l in lines:
-			ax.plot( l[0],l[1],alpha=0.2,color='gray')
+			ax.plot( l[0],l[1],alpha=alph,color='red')
 		ax.set_xlim([0,480])
 		ax.set_ylim([0,640])
 
@@ -173,8 +177,9 @@ def altaz_grid(im, spacing=10, verbose=False):
 
 
 def radec_grid(im,time, spacing=10,lat = 41.661):
+	#time is lst in degrees
 	ncp = lat
-	meridian = utc_to_lst(time)
+	meridian = time#utc_to_lst(time)
 
 	coords = []
 	for ra in np.arange(0,15,15):
--- src/pipeline.py
+++ src/pipeline.py
@@ -16,8 +16,8 @@ import conversions
 
 
 # -------  constants ---------
-HORIZON = 10
-MAG_LIM = 2.5
+HORIZON = 15
+MAG_LIM = 3.
 
 
 #load the test image
@@ -36,12 +36,16 @@ def pipe(fname, verbose=True, grid=True, names=True, points=False, save=False):
 	#im = np.rot90(im,1)
 
 	#store the utc time of the obervation, iowa city lat and long
-	time = head['date-obs']
-	print time
+	time_str = head['date-obs']
+	#time = '2018-04-18T08:17:33.591'
+	print time_str
+	time = Time(time_str) - 1*u.hour
+	lst = conversions.utc_to_lst(time_str) - (1./24 * 360)
 
 	#open the catalog
 	star_catalog = np.load('star_catalog.npy')
 	#print star_catalog[0]
+	#iowa_city = EarthLocation(lat=41.6611*u.deg, lon=-1*91.5302*u.deg,height=50.*u.m)
 
 	#using time, lat, and long, convert ra, dec to alt,az
 	#alt_az catalog
@@ -49,24 +53,27 @@ def pipe(fname, verbose=True, grid=True, names=True, points=False, save=False):
 	for kk,star in enumerate(star_catalog):
 		name, star_type, ra, dec, mag, epoch = star
 		ra = float(ra); dec = float(dec); mag = float(mag)
+		#c = SkyCoord(ra, dec, unit='deg')
+		#obj_altaz = c.transform_to(AltAz(obstime=time, location=iowa_city))
+		#alt = float("{0.alt:.3}".format(obj_altaz).split()[0]); az = float("{0.az:.3}".format(obj_altaz).split()[0]) 
 
 		#convert ra, dec to alt, az 
-		alt, az = conversions.radec_to_altaz(ra, dec, time,debug=False)
+		alt, az = conversions.radec_to_altaz(ra, dec, lst,debug=False)
 		if alt > HORIZON and mag < MAG_LIM:
 			#convert alt,az to x,y using Mason's fit
 			x,y = conversions.altaz_to_xy(alt, az)
 			sky_cat.append( [name, x,y, alt, az, ra, dec, mag] )
 
 
-	fig, ax = plt.subplots()
-	ax.imshow(-1*np.sqrt(im), origin='lower',cmap='gray')#,vmax=0, vmin=-0.5*np.max(np.sqrt(im)))
+	fig, ax = plt.subplots(figsize=(4,6))
+	ax.imshow(1*np.sqrt(im), origin='lower',cmap='gray',vmin=.4*np.max(np.sqrt(im)), vmax=1*np.max(np.sqrt(im)))
 	#plot objects with names
 	for kk, obj in enumerate(sky_cat):
 		name = obj[0]; x = obj[1]; y = obj[2]
 		if points:
 			ax.scatter(y,x,marker='*',color='cyan',alpha=0.5)
 		if names:
-			ax.text(y,x,name, fontsize=5)
+			ax.text(y,x,name, fontsize=5,alpha=0.9, color='gray')
 
 	
 	#plot alt,az grid (flag for ra,dec grid)
@@ -77,24 +84,34 @@ def pipe(fname, verbose=True, grid=True, names=True, points=False, save=False):
 				ax.add_patch(c)
 
 		for l in lines:
-			ax.plot( l[0],l[1],alpha=0.2,color='gray')
+			ax.plot( l[0],l[1],alpha=0.2,color='red')
 
 	ax.set_xlim([0,480])
 	ax.set_ylim([0,640])
+	plt.axis('off')
+	fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
 	if verbose:
 		plt.show()
 
 	#compare to see where nearest stars are
 	for kk, obj in enumerate(sky_cat):
 		x = obj[1]; y = obj[2]
-		#try:
-		yc,xc = conversions.centroid(np.power(im,2.),x,y,s=20, verbose=True)
+		try:
+			yc,xc = conversions.centroid(np.power(im,2.),x,y,s=20, verbose=False)
+		except:
+			print
 
 	#flag for image analysis -- edges, brightness, difference?
 
 	#save as a new file
 	if save:
 		plt.savefig(outname, bbinches='tight')
+
+	'''plt.close()
+	fig,ax = plt.subplots(2)
+	ax[0].scatter([x[3] for x in sky_cat], [x[5] for x in sky_cat])
+	ax[1].scatter([x[4] for x in sky_cat], [x[6] for x in sky_cat])
+	plt.show()'''
 
 def errfunc(p, *args):
 	a,b,c,d = p
@@ -156,7 +173,9 @@ def min_distances(fname):
 
 
 if __name__ == '__main__':
-	pipe('./assets/clear_with_shield.FIT', grid=True)
+	fname = './assets/075318.FIT'
+	#fname = './assets/clear_with_shield.FIT'
+	pipe(fname, grid=True,points=False,names=True,verbose=True)
 	#min_distances('./assets/clear_with_shield.FIT')
 
 
